facc9af0751acfdfa75aec2432a9e2addbb7c689
benjamin <benjamin.girard2005@gmail.com>
2026-01-14 15:36:34 +0100

    genererAlea

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-Le TP github copilot est terminer
+# Le TP github copilot est terminer
 Git Hub fonctionne aussi
--- test.c
+++ test.c
@@ -10,10 +10,14 @@ int minimum(int a, int b) {
     }
 }
 
+int genererNombreAleatoire(int maximum) {
+    return rand() % (maximum + 1);
+}
+
 int main(){
     srand(time(0));
-    int a = rand() % 1001;
-    int b = rand() % 1001;
+    int a = genererNombreAleatoire(1000);
+    int b = genererNombreAleatoire(1000);
     printf("a = %d, b = %d\n", a, b);
     printf("Le minimum est : %d\n", minimum(a, b));
     return 0;
